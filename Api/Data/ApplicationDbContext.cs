3f09a05e537765718abe562bed7d480e14edc31f
Ahmad Ezzeir - windows <aezzeir@gmail.com>
2020-07-22 15:08:07 +0300

    Add project files.

---
+++ Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
